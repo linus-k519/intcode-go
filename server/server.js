714feda04d4b99e773d890f0c18628bee72c94b4
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-12-22 20:56:03 +0100

    Finally receiving stdout from intcode on index.html

--- server/server.js
+++ server/server.js
@@ -18,24 +18,17 @@ app.get('/', (req, res) => {
     res.sendfile('index.html')
 })
 
-//Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-    console.log('A user connected')
-
+io.on('connection', socket => {
     socket.on('executeProgram', data => start_child(data, socket))
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('A user disconnected')
-    })
+    socket.on('disconnect', () => console.log('A user disconnected'))
 })
 
 function start_child(data, socket) {
     tmp.file(undefined, (err, path, fd, cleanup) => {
         fs.writeFileSync(path, data.program)
         const child = childProcess.spawn('../intcode', ['-stats', path])
-        child.stdout.on('data', chunk => socket.emit('stdout', chunk))
-        child.stderr.on('data', chunk => socket.emit('stdout', chunk))
+        child.stdout.on('data', chunk => socket.emit('stdout', chunk.toString()))
+        child.stderr.on('data', chunk => socket.emit('stdout', chunk.toString()))
         child.stdout.on('end', () => {
             cleanup()
             socket.emit('endProgram', 'Program end')
